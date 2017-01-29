ff91fae753a64696063977f9166472beea69e77e
Ben Marshall <bm12656@my.bristol.ac.uk>
2017-01-29 12:47:16 +0000

    Remove old single-cycle specific code from the decoder and add constants file.  On branch master  Your branch is ahead of 'origin/master' by 2 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   main/rv32ui_decoder.v
    	new file:   main/rvm_constants.v

--- rtl/main/rv32ui_decoder.v
+++ rtl/main/rv32ui_decoder.v
@@ -1,6 +1,6 @@
 
 //
-// RISCV single-cycle implementation.
+// RISCV multi-cycle implementation.
 //
 // Module:      rv32ui_decoder.v
 //
@@ -8,7 +8,7 @@
 //              and decodes it into the relevent data and control lines.
 //
 
-`include "rvs_constants.v"
+`include "rvm_constants.v"
 
 module rv32ui_decoder(
 input  wire [`WORD_W    -1:0] inputword, // The word to decode.
@@ -19,10 +19,8 @@ output wire                   illegal_instr, // No valid instruction decoded.
 output wire [`REG_ADDR_W-1:0] rs1      , // Source register 1.
 output wire [`REG_ADDR_W-1:0] rs2      , // Source register 2.
 output wire [`REG_ADDR_W-1:0] dest     , // Destination register.
-output wire [`WORD_W    -1:0] imm      , // Decoded immediate.
+output wire [`WORD_W    -1:0] imm        // Decoded immediate.
 
-output wire [`RVS_FU_TYPE  ] fu_type  , // Type of FU the instruction needs.
-output wire [`RVS_FU_OPCODE] fu_opcode  // Operation the FU should perform.
 );
 
 // Use enc for internal references to input word. It is isolated such that it
@@ -217,77 +215,8 @@ assign imm = {`WORD_W{itype_i }} & {{21{enc[31]}},enc[31:20]               } |
              {`WORD_W{itype_u }} & {enc[31:12] , 12'b0                     } |
              {`WORD_W{itype_uj}} & {{12{enc[31]}}, enc[19:12], enc[20],enc[30:21],1'b0};
 
-// Which functional unit should the instruction be sent to?
-wire use_fu_pcu = gen_jal | gen_jalr | gen_bne | gen_beq | gen_bltu | gen_blt |
-                  gen_bge | gen_bgeu | gen_eret ;
-
-wire use_fu_alu = gen_lui   | gen_auipc | itype_r | gen_andi | gen_addi | 
-                  gen_sltiu | gen_slti  | gen_ori | gen_xori | gen_slli | 
-                  gen_srli  | gen_srai  ;
-
-wire use_fu_lsu = gen_lw    | gen_lh    | gen_lhu | gen_lb   | gen_lbu  |
-                  gen_sw    | gen_sh    | gen_sb  ;
-
-wire use_fu_scu = gen_csrrw  | gen_csrrc  | gen_csrrs | gen_csrrci |
-                  gen_csrrsi | gen_csrrwi ;
-
-assign fu_type = `RVS_FU_PCU & {3{use_fu_pcu}} |
-                 `RVS_FU_ALU & {3{use_fu_alu}} |
-                 `RVS_FU_LSU & {3{use_fu_lsu}} |
-                 `RVS_FU_SCU & {3{use_fu_scu}} ;
-
-// Which opcode should be sent to the functional unit?
-wire [4:0] alu_opcode = {5{gen_lui   }} & `RVS_ALU_LUI    |
-                  {5{gen_auipc }} & `RVS_ALU_AUIPC  |
-                  {5{gen_add   }} & `RVS_ALU_ADD    |
-                  {5{gen_slt   }} & `RVS_ALU_SLT    |
-                  {5{gen_sltu  }} & `RVS_ALU_SLTU   |
-                  {5{gen_and   }} & `RVS_ALU_AND    |
-                  {5{gen_or    }} & `RVS_ALU_OR     |
-                  {5{gen_xor   }} & `RVS_ALU_XOR    |
-                  {5{gen_sll   }} & `RVS_ALU_SLL    |
-                  {5{gen_srl   }} & `RVS_ALU_SRL    |
-                  {5{gen_sub   }} & `RVS_ALU_SUB    |
-                  {5{gen_sra   }} & `RVS_ALU_SRA    |
-                  {5{gen_andi  }} & `RVS_ALU_ANDI   |
-                  {5{gen_addi  }} & `RVS_ALU_ADDI   |
-                  {5{gen_sltiu }} & `RVS_ALU_SLTIU  |
-                  {5{gen_slti  }} & `RVS_ALU_SLTI   |
-                  {5{gen_ori   }} & `RVS_ALU_ORI    |
-                  {5{gen_xori  }} & `RVS_ALU_XORI   |
-                  {5{gen_slli  }} & `RVS_ALU_SLLI   |
-                  {5{gen_srli  }} & `RVS_ALU_SRLI   |
-                  {5{gen_srai  }} & `RVS_ALU_SRAI   ;
-                                                    
-wire [4:0] pcu_opcode = {5{gen_jal   }} & `RVS_PCU_JAL    |
-                  {5{gen_jalr  }} & `RVS_PCU_JALR   |
-                  {5{gen_beq   }} & `RVS_PCU_BEQ    |
-                  {5{gen_bne   }} & `RVS_PCU_BNE    |
-                  {5{gen_bltu  }} & `RVS_PCU_BLTU   |
-                  {5{gen_blt   }} & `RVS_PCU_BLT    |
-                  {5{gen_bge   }} & `RVS_PCU_BGE    |
-                  {5{gen_bgeu  }} & `RVS_PCU_BGEU   |
-                  {5{gen_eret  }} & `RVS_PCU_ERET   ;
-                                                    
-wire [4:0] lsu_opcode = {5{gen_lw    }} & `RVS_LSU_LW     |
-                  {5{gen_sw    }} & `RVS_LSU_SW     |
-                  {5{gen_lh    }} & `RVS_LSU_LH     |
-                  {5{gen_lhu   }} & `RVS_LSU_LHU    |
-                  {5{gen_sh    }} & `RVS_LSU_SH     |
-                  {5{gen_lb    }} & `RVS_LSU_LB     |
-                  {5{gen_lbu   }} & `RVS_LSU_LBU    |
-                  {5{gen_sb    }} & `RVS_LSU_SB     ;
-
-wire [4:0] scu_opcode = {5{gen_csrrs }} & `RVS_SCU_CSRRS  |
-                  {5{gen_csrrc }} & `RVS_SCU_CSRRC  |
-                  {5{gen_csrrw }} & `RVS_SCU_CSRRW  |
-                  {5{gen_csrrsi}} & `RVS_SCU_CSRRSI |
-                  {5{gen_csrrci}} & `RVS_SCU_CSRRCI |
-                  {5{gen_csrrwi}} & `RVS_SCU_CSRRWI ;
-
-assign fu_opcode = {5{use_fu_pcu}} & pcu_opcode |
-                   {5{use_fu_lsu}} & lsu_opcode |
-                   {5{use_fu_alu}} & alu_opcode |
-                   {5{use_fu_scu}} & scu_opcode ;
+//
+// TODO: Decoder outputs to decide which instruction to execute.
+//
 
 endmodule
---
+++ rtl/main/rvm_constants.v
@@ -0,0 +1,9 @@
+
+//
+// RISCV multi-cycle implementation.
+//
+// Module:      N/A
+//
+// Description: Contains all compile time constants for the core.
+//
+//
